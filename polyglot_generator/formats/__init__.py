11ba91b1c5aeee4f87ef9add567e5d95dfa438d7
hakmad <hakmad@fastmail.com>
2023-03-09 13:49:47 +0000

    Added docstrings to formats module
    Included extra details about the different options for creating new
    formats and hosting parasites in files.

--- polyglot_generator/formats/__init__.py
+++ polyglot_generator/formats/__init__.py
@@ -1,8 +1,40 @@
+"""Formats module.
+
+This module contains a template File class that can be extended for different
+formats.
+"""
+
 __all__ = []
 
 
 class File:
+    """Container class for other formats.
+
+    Designed to be extended to other formats.
+
+    Attributes:
+        name (str): name of the file.
+        formats (str): format of the file.
+        data (bytes): data of the file.
+        size (int): size (in bytes) of the file.
+
+        supports_stack_before_sof (bool): does the format support data before
+            the start of file marker?
+        supports_stack_after_eof (bool): does the format support data after the
+            end of file marker?
+
+        supports_parasite (bool): does the format support parasite data
+            structures?
+        max_parasite_size (int): maximum size for a parasite.
+    """
+
     def __init__(self, name, format):
+        """Create a new File object.
+
+        Args:
+            name (str): name of the file.
+            format (str): format of the file.
+        """
         # Metadata.
         self.name = name
         self.format = format
@@ -22,4 +54,20 @@ class File:
         self.max_parasite_size = 0
 
     def host_parasite(self, parasite):
+        """Host a parasite in the current file.
+
+        This method is intended to be called from the host file.
+
+        This method is intended to be overridden by any classes that extend
+        this format (given that the format supports creating parasites).
+
+        Args:
+            parasite (File): the parasite file to host within the current file.
+
+        Examples:
+            host = File(...)
+            parasite = File(...)
+            ...
+            polyglot = host.host_parasite(parasite)
+        """
         pass
