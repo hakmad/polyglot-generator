d53640b9fd15a1ffe59f4ad146b52da31794b9f1
hakmad <hakmad@fastmail.com>
2023-03-09 15:06:36 +0000

    Added exceptions to polyglot_generator.py
    Might extend these exceptions to be more detailed/granular. Might also
    need to create additional ways to create File objects - from a file name
    or from some bytes directly.

--- polyglot_generator/polyglot_generator.py
+++ polyglot_generator/polyglot_generator.py
@@ -4,6 +4,16 @@ Provides functions to create polyglots.
 """
 
 
+class StackNotViableException(Exception):
+    """Raised if a stack is not viable with the two given files/formats."""
+    pass
+
+
+class ParasiteNotViableException(Exception):
+    """Raised if a parasite is not viable with the two given files/formats."""
+    pass
+
+
 def create_stack(top, bottom):
     """Create a stack polyglot file.
 
@@ -11,6 +21,14 @@ def create_stack(top, bottom):
         top (File): top file for the polyglot.
         bottom (File): bottom file for the polyglot.
     """
+    if not top.supports_stack_after_eof:
+        raise StackNotViableException(f"Top file {top.name} ({top.format}) does
+            not support data after EOF!")
+
+    if not bottom.supports_stack_before_sof:
+        raise StackNotViableException(f"Bottom file {bottom.name}
+            ({bottom.format}) does not support data before EOF!")
+
     return top.data + bottom.data
 
 
@@ -21,4 +39,13 @@ def create_parasite(host, parasite):
         host (file): host file for the polyglot.
         parasite (file): parasite file for the polyglot.
     """
+    if not host.supports_parasite:
+        raise ParasiteNotViableException(f"Host file {host.name} 
+            ({host.format}) does not support parasites!")
+
+    if not parasite.size > host.max_parasite_size:
+        raise ParasiteNotViableException(f"Parasite file {parasite.name}
+            ({parasite.format}) exceeds max parasite size for host format
+            {host.format}!")
+
     return host.host_parasite(parasite)
